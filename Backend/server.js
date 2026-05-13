913ec3c96423733da420f65adf5860f0ab5d2b42
manuk1385 <manukees13@gmail.com>
2026-05-12 23:44:05 -0300

    inicio mvp

---
+++ Backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API funcionando");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
